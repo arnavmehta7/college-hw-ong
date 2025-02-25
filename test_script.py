cb815913e2d18c28c3b43b129fa21c01bc7a4c3d
arnavmehta7 <arnavmehta.cs@gmail.com>
2025-02-26 02:23:45 +0530

    added images

--- test_script.py
+++ test_script.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+import cv2
+
+img = cv2.imread('plaksha_Faculty.jpg')
+img2 = cv2.imread('Dr_Shashi_Tharoor.jpg')
+
+assert img is not None, "Image 1 is not loaded"
+assert img2 is not None, "Image 2 is not loaded"
+
+print("Images loaded successfully")
+
