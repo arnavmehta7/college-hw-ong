d5e4753fb02dab4d9894f0d157fd7ffefe75d324
arnavmehta7 <arnavmehta.cs@gmail.com>
2025-02-26 02:41:27 +0530

    resolved the bug for testing script

--- test_script.py
+++ test_script.py
@@ -1,7 +1,7 @@
 import cv2
 
-img = cv2.imread('plaksha_Faculty.jpg')
-img2 = cv2.imread('Dr_Shashi_Tharoor.jpg')
+img = cv2.imread('./plaksha_Faculty.jpg')
+img2 = cv2.imread('./Dr_Shashi_Tharoor.jpg')
 
 assert img is not None, "Image 1 is not loaded"
 assert img2 is not None, "Image 2 is not loaded"
